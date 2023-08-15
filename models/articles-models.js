63e0627ca3a663d4c2a9019af926f3f37f455af5
Oliver Crossley <ollythecrossley@outlook.com>
2023-08-15 13:43:52 +0100

    Fixed small caps error on query

--- models/articles-models.js
+++ models/articles-models.js
@@ -4,7 +4,7 @@ exports.selectArticles = () => {
     return db.query(`
         SELECT articles.author, title, articles.article_id, articles.body, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.article_id)::INT AS comment_count
         FROM articles
-                 LEFT JOIN comments on articles.article_id = comments.article_id
+                 LEFT JOIN comments ON articles.article_id = comments.article_id
         GROUP BY articles.author, title, articles.article_id, articles.body, topic, articles.created_at, articles.votes, article_img_url
     `).then(({rows}) => {
         return rows
