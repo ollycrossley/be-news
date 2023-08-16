d6820c75ba5180438f122c2ee143278bbe787025
Oliver Crossley <ollythecrossley@outlook.com>
2023-08-16 12:35:36 +0100

    removes console log in errorHandler

--- db/error-handler.js
+++ db/error-handler.js
@@ -1,5 +1,4 @@
 exports.errorHandler = (err, req, res, next) => {
-    console.log(err)
     if (err.status && err.msg) {
         res.status(err.status).send({ msg: err.msg });
     } else if (err.code === "22P02") {
