fa7578e235688c65fadab9f91cbbd90b2dda0f89
Oliver Crossley <ollythecrossley@outlook.com>
2023-08-17 11:33:22 +0100

    adjusts controllers to return body in key value pair with tests

--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -2,27 +2,27 @@ const {selectArticleById, selectArticles, selectArticleComments, insertArticleCo
 
 exports.getArticles = (req, res, next) => {
     selectArticles().then(articles => {
-        res.status(200).send(articles)
+        res.status(200).send({articles})
     }).catch(err => next(err))
 }
 exports.getArticleById = (req, res, next) => {
     const {article_id} = req.params
     selectArticleById(article_id).then(article => {
-        res.status(200).send(article)
+        res.status(200).send({article})
     }).catch(err => next(err))
 }
 exports.getArticleComments = (req, res, next) => {
     const {article_id} = req.params
     selectArticleComments(article_id).then(comments => {
-        res.status(200).send(comments)
+        res.status(200).send({comments})
     }).catch(err => next(err))
 }
 
 exports.postArticleComment = (req, res, next) => {
     const {article_id} = req.params
     const comment = req.body
-    insertArticleComment(article_id, comment).then(article => {
-        res.status(201).send(article)
+    insertArticleComment(article_id, comment).then(comment => {
+        res.status(201).send({comment})
     }).catch(err => next(err))
 }
 
