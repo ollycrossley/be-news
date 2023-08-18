c4d102880f3e4f0942f8b11081710016421b5168
Oliver Crossley <ollythecrossley@outlook.com>
2023-08-18 15:56:02 +0100

    Allows querying of topics

--- controllers/topics-controllers.js
+++ controllers/topics-controllers.js
@@ -1,7 +1,8 @@
 const {selectTopics} = require("../models/topics-models");
 
 exports.getTopics = (req, res, next) => {
-    selectTopics().then(topics => {
+    const {topic} = req.query || null
+    selectTopics(topic).then(topics => {
         res.status(200).send({topics})
     }).catch(err => next(err))
 }
--- models/topics-models.js
+++ models/topics-models.js
@@ -1,7 +1,13 @@
 const db = require('../db/connection')
 
-exports.selectTopics = () => {
-    return db.query(`SELECT * FROM topics`).then(({rows}) => {
+exports.selectTopics = (topic) => {
+    return db.query(`
+        SELECT *
+        FROM topics
+        WHERE $1::VARCHAR IS NULL OR slug = $1::VARCHAR`, [topic]).then(({rows}) => {
+        if (rows.length === 0) {
+            return Promise.reject({status: 404, msg: "topic does not exist"})
+        }
         return rows
     })
 }
